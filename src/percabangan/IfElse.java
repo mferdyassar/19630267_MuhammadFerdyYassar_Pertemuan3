bd9cc922c94322b6eedefdb19a630c9183063346
mferdyassar <mferdyassar@gmail.com>
2021-04-21 00:25:05 +0800

    Percabangan Ifelse bernilai condition1 false

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -3,12 +3,15 @@ package Percabangan;
 public class IfElse {
     public static void main(String[] args){
         double totalBelanja = 40000;
-        double uangDiDompet = 80000;
+        double uangDiDompet = 30000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi belanjaan anda");
+        }else if(uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang Cukup, angsul : "+angsul);
         }else{
-            System.out.println("Uang cukup banar... Selamat Menikmati..");
+            System.out.println("Uang pas banar... Selamat Menikmati..");
         }
     }
 }
