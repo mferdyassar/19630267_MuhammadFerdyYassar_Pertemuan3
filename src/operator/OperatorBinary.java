317769e0c1b38ebfe24c9796988bd017757d3130
mferdyassar <mferdyassar@gmail.com>
2021-04-21 00:13:30 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -9,6 +9,13 @@ public class OperatorBinary {
     String nama = "Muhammad Ferdy Yassar";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.98;
-    
+
+    public static void main(String[] args){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
     }
 }
