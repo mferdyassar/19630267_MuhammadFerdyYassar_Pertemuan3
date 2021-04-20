6c31b9acd99bdf633552e5d342ab9b916c821612
mferdyassar <mferdyassar@gmail.com>
2021-04-21 00:12:53 +0800

    Operator Binary Assignment

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -9,13 +9,6 @@ public class OperatorBinary {
     String nama = "Muhammad Ferdy Yassar";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.98;
-
-    public static void main(String[] args){
-        System.out.println(9+3);
-        System.out.println(9-3);
-        System.out.println(9*3);
-        System.out.println(9/3);
-        System.out.println(9%3);
-        System.out.println(9%2);
+    
     }
 }
