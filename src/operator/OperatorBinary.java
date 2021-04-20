67b88768e5f544df4da190742192859d96ca7a8d
mferdyassar <mferdyassar@gmail.com>
2021-04-21 00:11:58 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,21 @@
+package Operator;
+
+public class OperatorBinary {
+
+    //operator (-) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Muhammad Ferdy Yassar";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.98;
+
+    public static void main(String[] args){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
