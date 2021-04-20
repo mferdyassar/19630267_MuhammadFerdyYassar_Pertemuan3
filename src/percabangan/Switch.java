6ee8ffc750aea18f16dd8e158d4d992e06184809
mferdyassar <mferdyassar@gmail.com>
2021-04-21 00:29:09 +0800

    Percabangan Switch nilai A

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -2,19 +2,27 @@ package Percabangan;
 
 public class Switch {
     public static void main(String[] args){
-        int jalanJalan=2;
-        switch(jalanJalan){
-            case '1':
+        char nilai='A';
+        switch(nilai){
+            case 'A':
                 System.out.println("Mhs    : \"Terima kasih pak\"");
+                System.out.println("Dosen  : \"Selamat ya !!\"");
                 break;
-            case '2':
+            case 'B':
                 System.out.println("Mhs    : \"Kenapa ulun kada dapat A, Pak?\"");
+                System.out.println("Dosen  : \"!@#$%\"");
                 break;
-            case '3':
+            case 'C':
                 System.out.println("Mhs    : \"Ulun turun tarus pak ai tugas gawi tarus :(\"");
+                System.out.println("Dosen  : \"Tapi bisa lah menjawab ujian\"");
+                System.out.println("Mhs    : \"kada pak ai :D\"");
                 break;
             default:
                 System.out.println("Mhs    : \"Ulun turun tarus pak ai tugas gawi jua\"");
+                System.out.println("Dosen  : \"Bujur jua kah>\"");
+                System.out.println("Dosen  : \"*Melihati Berkas\"");
+                System.out.println("Dosen  : \"*Menceleng\"");
+                System.out.println("Mhs    : \"Kaburrr...\"");
                 break;
         }
     }
